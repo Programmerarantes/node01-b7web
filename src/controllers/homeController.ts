1d3a08bb5d41c968eb4908fbb567d237f131de44
Matheus <chaves.ma1993@gmail.com>
2024-02-05 18:50:21 -0300

    inserindo dados sequelize

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,14 +1,31 @@
 import { Request, Response } from "express";
-
+import { Op } from 'sequelize';
 import { Product } from '../models/Product';
 import { User } from "../models/User";
 
-export const home = async (req: Request, res: Response)=> {
-    let users = await User.findAll({
-        attributes: ['nome', 'cpf', 'data_nascimento']
 
-    });
-    console.log("Usuários:", JSON.stringify(users))
+export const home = async (req: Request, res: Response)=> {
+    //build + save
+    /*const user = User.build({
+        id: '55',
+        nome: "Fulaninho",
+        cpf: '1599514781',
+        rg: '7778881112',
+        data_nascimento: '2021-05-25',
+        sexo: "Outros"
+    })
+    await user.save()
+
+    //ceate
+    const user = await User.create({
+        nome: "Sr Miague Xavi ",
+        cpf: '9988443333',
+        rg: '1771881221',
+        data_nascimento: '1999-10-10',
+        sexo: "Masculino"
+    })
+    */
+
 
     let age: number = 51;
     let showOld: boolean = false;
@@ -26,8 +43,7 @@ export const home = async (req: Request, res: Response)=> {
         lastname: "Chaves",
         showOld,
         products: list,
-        expensives: expensiveList,
-        users
+        expensives: expensiveList
     });
 
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,25 @@
 import {Model, DataTypes } from 'sequelize';
 import { sequelize } from '../instances/pg';
 
+interface DataNascimento {
+    dia: number;
+    mes: number;
+    ano: number;
+}
+
 interface UserInstance extends Model {
     id: number,
     nome: string,
     cpf: string,
     rg: string, 
-    data_nascimento: Date,
+    data_nascimento: DataNascimento,
     sexo: string
 }
 
 export const User = sequelize.define<UserInstance>("User", {
     id: {
         primaryKey: true,
+        autoIncrement: true,
         type: DataTypes.INTEGER
     },
     nome: {
